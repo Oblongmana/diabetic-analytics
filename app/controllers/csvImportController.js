b8272ffc2cdbf509da5cb23ff65fcb4c9933a586
James Hill <oblongmana@gmail.com>
2015-09-08 09:43:54 +1200

    Remove comment cruft from csv controller

--- app/controllers/csvImportController.js
+++ app/controllers/csvImportController.js
@@ -2,13 +2,6 @@
   'use strict';
 
   var angular = require('angular');
-  // var papaparse = require('papaparse');
-  // var angularFile = require('angular-file');
-  // var jquery = require('jquery');
-  
-  // Note that the service provided by require('angular-resource') is in fact
-  //  $resource, so while the app does require('angular-resource'), we can't 
-  //  programmatically get the name of the service, so hard-code $resource in :/
 
   angular.module('diabeticAnalytics').controller('csvImportController',
     [
